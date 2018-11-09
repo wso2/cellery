da377d94188c2a876eeec90bd1e437b5b499e501
NipunaPrashan <nipunaprashan99@gmail.com>
2018-11-09 11:29:10 +0530

    Update build command with flags

--- cmd/build.go
+++ cmd/build.go
@@ -3,41 +3,54 @@ package main
 import (
 	"fmt"
 	"github.com/spf13/cobra"
+	"github.com/tj/go-spin"
 	"os/exec"
 	"bufio"
 	"os"
+	"strings"
+	"time"
 )
 
 func newBuildCommand() *cobra.Command {
-	var t string
+	var tag string
 	var descriptorName string
 	cmd := &cobra.Command{
 		Use:   "build [OPTIONS]",
 		Short: "Build an immutable cell image with required dependencies",
 		RunE: func(cmd *cobra.Command, args []string) error {
-			err := runBuild(t, descriptorName)
+			if (len(args) == 0) {
+				cmd.Help()
+				return nil
+			}
+			descriptorName = args[0]
+			err := runBuild(tag, descriptorName)
 			if err != nil{
 				cmd.Help()
 				return err
 			}
 			return nil
 		},
+		Example: "  cellery build my-project-1.0.0.cell -t myproject",
 	}
-	cmd.Flags().StringVarP(&t, "tag", "t", "", "Name and optionally a tag in the 'name:tag' format")
-	cmd.Flags().StringVarP(&descriptorName, "descriptor", "d", "", "Cell descriptor name")
+	cmd.Flags().StringVarP(&tag, "tag", "t", "", "Name and optionally a tag in the 'name:tag' format")
 	return cmd
 }
 
 func runBuild(tag string, descriptorName string) error {
 	if tag == "" {
-		return fmt.Errorf("no build tag specified")
+		tag = strings.Split(descriptorName, ".")[0]
 	}
 	if descriptorName == "" {
 		return fmt.Errorf("no descriptor name specified")
 	}
 
-	fmt.Printf("Building %q\n", tag)
-	//s := spin.New()
+	s := spin.New()
+	for i := 0; i < 40; i++ {
+		fmt.Printf("\r\033[36m%s\033[m Building %s %q", s.Next(), "image", tag)
+		time.Sleep(100 * time.Millisecond)
+	}
+	fmt.Printf("\n")
+
 	cmd := exec.Command("ballerina", "build", descriptorName)
 	stdoutReader, _ := cmd.StdoutPipe()
 	stdoutScanner := bufio.NewScanner(stdoutReader)
@@ -63,26 +76,8 @@ func runBuild(tag string, descriptorName string) error {
 		fmt.Printf("\x1b[31;1m Cell build finished with error: \x1b[0m %v \n", err)
 		os.Exit(1)
 	}
-	//for i := 0; i < 40; i++ {
-	//	fmt.Printf("\r\033[36m%s\033[m Building %s %q", s.Next(), "image", tag)
-	//	time.Sleep(100 * time.Millisecond)
-	//}
+
 	fmt.Printf("\r\033[32m Successfully built cell image \033[m %q \n", tag)
-	//for i := 0; i < 80; i++ {
-	//	fmt.Printf("\r\033[36m%s\033[m Building %q", s.Next(), "STS")
-	//	time.Sleep(100 * time.Millisecond)
-	//}
-	//fmt.Printf("\r\033[32mBuild success %q\033[m\n", "STS")
-	//for i := 0; i < 100; i++ {
-	//	fmt.Printf("\r\033[36m%s\033[m Building %q", s.Next(), "employee-service")
-	//	time.Sleep(100 * time.Millisecond)
-	//}
-	//fmt.Printf("\r\033[32mBuild success %q\033[m\n", "employee-service")
-	//for i := 0; i < 70; i++ {
-	//	fmt.Printf("\r\033[36m%s\033[m Building %q", s.Next(), "salary-service")
-	//	time.Sleep(100 * time.Millisecond)
-	//}
-	//fmt.Printf("\r\033[32mBuild success %q\033[m\n", "salary-service")
-	//fmt.Printf("Generated cell image %q a70ad572a50f\n",tag)
+
 	return nil
 }

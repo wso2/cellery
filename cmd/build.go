4b40250bea2f3f70b934f3e76955944bc8b1e24d
NipunaPrashan <nipunaprashan99@gmail.com>
2018-11-09 13:44:30 +0530

    Modify init and build

--- cmd/build.go
+++ cmd/build.go
@@ -3,18 +3,19 @@ package main
 import (
 	"fmt"
 	"github.com/spf13/cobra"
-	"github.com/tj/go-spin"
-	"time"
-	
+	"os/exec"
+	"bufio"
+	"os"
 )
 
 func newBuildCommand() *cobra.Command {
 	var t string
+	var descriptorName string
 	cmd := &cobra.Command{
 		Use:   "build [OPTIONS]",
 		Short: "Build an immutable cell image with required dependencies",
 		RunE: func(cmd *cobra.Command, args []string) error {
-			err := runBuild(t)
+			err := runBuild(t, descriptorName)
 			if err != nil{
 				cmd.Help()
 				return err
@@ -23,36 +24,65 @@ func newBuildCommand() *cobra.Command {
 		},
 	}
 	cmd.Flags().StringVarP(&t, "tag", "t", "", "Name and optionally a tag in the 'name:tag' format")
+	cmd.Flags().StringVarP(&descriptorName, "descriptor", "d", "", "Cell descriptor name")
 	return cmd
 }
 
-func runBuild(tag string) error {
+func runBuild(tag string, descriptorName string) error {
 	if tag == "" {
 		return fmt.Errorf("no build tag specified")
 	}
+	if descriptorName == "" {
+		return fmt.Errorf("no descriptor name specified")
+	}
 
-	fmt.Printf("Generating %q\n", tag)
-	s := spin.New()
-	for i := 0; i < 40; i++ {
-		fmt.Printf("\r\033[36m%s\033[m Building %q", s.Next(), "Gateway")
-		time.Sleep(100 * time.Millisecond)
+	fmt.Printf("Building %q\n", tag)
+	//s := spin.New()
+	cmd := exec.Command("ballerina", "build", descriptorName)
+	stdoutReader, _ := cmd.StdoutPipe()
+	stdoutScanner := bufio.NewScanner(stdoutReader)
+	go func() {
+		for stdoutScanner.Scan() {
+			fmt.Println(stdoutScanner.Text())
+		}
+	}()
+	stderrReader, _ := cmd.StderrPipe()
+	stderrScanner := bufio.NewScanner(stderrReader)
+	go func() {
+		for stderrScanner.Scan() {
+			fmt.Println(stderrScanner.Text())
+		}
+	}()
+	err := cmd.Start()
+	if err != nil {
+		fmt.Printf("Error in executing cell build: %v \n", err)
+		os.Exit(1)
 	}
-	fmt.Printf("\r\033[32mBuild success %q\033[m\n", "Gateway")
-	for i := 0; i < 80; i++ {
-		fmt.Printf("\r\033[36m%s\033[m Building %q", s.Next(), "STS")
-		time.Sleep(100 * time.Millisecond)
+	err = cmd.Wait()
+	if err != nil {
+		fmt.Printf("\x1b[31;1m Cell build finished with error: \x1b[0m %v \n", err)
+		os.Exit(1)
 	}
-	fmt.Printf("\r\033[32mBuild success %q\033[m\n", "STS")
-	for i := 0; i < 100; i++ {
-		fmt.Printf("\r\033[36m%s\033[m Building %q", s.Next(), "employee-service")
-		time.Sleep(100 * time.Millisecond)
-	}
-	fmt.Printf("\r\033[32mBuild success %q\033[m\n", "employee-service")
-	for i := 0; i < 70; i++ {
-		fmt.Printf("\r\033[36m%s\033[m Building %q", s.Next(), "salary-service")
-		time.Sleep(100 * time.Millisecond)
-	}
-	fmt.Printf("\r\033[32mBuild success %q\033[m\n", "salary-service")
-	fmt.Printf("Generated cell image %q a70ad572a50f\n",tag)
+	//for i := 0; i < 40; i++ {
+	//	fmt.Printf("\r\033[36m%s\033[m Building %s %q", s.Next(), "image", tag)
+	//	time.Sleep(100 * time.Millisecond)
+	//}
+	fmt.Printf("\r\033[32m Successfully built cell image \033[m %q \n", tag)
+	//for i := 0; i < 80; i++ {
+	//	fmt.Printf("\r\033[36m%s\033[m Building %q", s.Next(), "STS")
+	//	time.Sleep(100 * time.Millisecond)
+	//}
+	//fmt.Printf("\r\033[32mBuild success %q\033[m\n", "STS")
+	//for i := 0; i < 100; i++ {
+	//	fmt.Printf("\r\033[36m%s\033[m Building %q", s.Next(), "employee-service")
+	//	time.Sleep(100 * time.Millisecond)
+	//}
+	//fmt.Printf("\r\033[32mBuild success %q\033[m\n", "employee-service")
+	//for i := 0; i < 70; i++ {
+	//	fmt.Printf("\r\033[36m%s\033[m Building %q", s.Next(), "salary-service")
+	//	time.Sleep(100 * time.Millisecond)
+	//}
+	//fmt.Printf("\r\033[32mBuild success %q\033[m\n", "salary-service")
+	//fmt.Printf("Generated cell image %q a70ad572a50f\n",tag)
 	return nil
 }
--- cmd/init.go
+++ cmd/init.go
@@ -2,14 +2,14 @@ package main
 
 import (
 	"bufio"
-	"bytes"
-	"encoding/json"
+	//"bytes"
+	//"encoding/json"
 	"fmt"
 	"github.com/fatih/color"
 	i "github.com/oxequa/interact"
 	"github.com/spf13/cobra"
-	"io/ioutil"
-	"net/http"
+	//"io/ioutil"
+	//"net/http"
 	"os"
 	"path/filepath"
 )
@@ -82,31 +82,59 @@ func runInit() error {
 		},
 	})
 
-
-	type Payload struct {
-		Name    string `json:"name"`
-		Version string `json:"version"`
-	}
-	payload := Payload{}
-	payload.Name = projectName
-	payload.Version = projectVersion
-
-	bytesPayload, err := json.Marshal(payload)
-
-	// Call API server to get the version details.
-	resp, err := http.Post(BASE_URL + "/cell/init", "application/json", bytes.NewBuffer(bytesPayload))
-	if err != nil {
-		fmt.Println("Error connecting to the cellery api server.")
-	}
-	defer resp.Body.Close()
-	body, err := ioutil.ReadAll(resp.Body)
+	// hard coding the cell format for now
+	cellTemplate :=
+	"import wso2/cellery; \n" +
+	"// my-project \n" +
+	"cellery:Component my-project = { \n" +
+		"   name: 'my-project', \n" +
+		"   source: {\n" +
+		"      dockerImage: 'docker.io/my-project:v1' \n" +
+		"   }, \n" +
+		"   replicas: { \n" +
+		"      min: 1, \n" +
+		"      max: 1 \n" +
+		"   }, \n" +
+		"   container:{}, \n" +
+		"   env: {}, \n" +
+		"   apis: {}, \n" +
+		"   dependencies: {}, \n" +
+		"   security: {} \n" +
+	"};"
+	//type Payload struct {
+	//	Name    string `json:"name"`
+	//	Version string `json:"version"`
+	//}
+	//payload := Payload{}
+	//payload.Name = projectName
+	//payload.Version = projectVersion
+	//
+	//bytesPayload, err := json.Marshal(payload)
+	//
+	//// Call API server to get the version details.
+	//resp, err := http.Post(BASE_URL + "/cell/init", "application/json", bytes.NewBuffer(bytesPayload))
+	//if err != nil {
+	//	fmt.Println("Error connecting to the cellery api server.")
+	//}
+	//defer resp.Body.Close()
+	//body, err := ioutil.ReadAll(resp.Body)
 
 	// Get current directory
 	dir, err := filepath.Abs(filepath.Dir(os.Args[0]))
+	if err != nil {
+		fmt.Println("Error in getting current directory location: " + err.Error());
+		os.Exit(1)
+	}
 
 	file, err := os.Create(dir + "/" + projectName+ "-" + projectVersion + ".cell")
+	if err != nil {
+		fmt.Println("Error in creating file: " + err.Error());
+		os.Exit(1)
+	}
+	defer file.Close();
 	w := bufio.NewWriter(file)
-	w.WriteString(string(body))
+	//w.WriteString(string(body))
+	w.WriteString(fmt.Sprintf("%s", cellTemplate))
 	w.Flush()
 
 	fmt.Println("Initialized cell project in dir: " + faintWhite(dir))

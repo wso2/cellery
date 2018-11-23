fa6e56a39f8a0cd8f6bc6abeca56f49e5b7e6eb3
NipunaPrashan <nipunaprashan99@gmail.com>
2018-11-23 17:08:36 +0530

    Use multi-stage docker build for registry apis

--- registry/build.sh
+++ registry/build.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/usr/bin/env bash
 # --------------------------------------------------------------------
 # Copyright (c) 2018, WSO2 Inc. (http://wso2.com) All Rights Reserved.
 #
@@ -15,20 +15,15 @@
 # limitations under the License.
 # -----------------------------------------------------------------------
 
-RED='\033[0;31m'
-GREY='\033[0;37m'
-YELLOW='\033[0;33m'
-BOLD='\033[1m'
-NC='\033[0m'
 
 function showUsageAndExit() {
-    echo "${GREY}${BOLD}USAGE${NC}"
+    echo "USAGE$"
     echo "  $ ./build.sh [OPTIONS]"
     echo
-    echo "${GREY}${BOLD}OPTIONS${NC}"
+    echo "OPTIONS"
     echo "  -v\t(OPTIONAL) Build version. If not specified a default value (0.0.1) will be used."
     echo
-    echo "${GREY}${BOLD}EXAMPLES${NC}"
+    echo "EXAMPLES"
     echo "  $ ./build.sh -v 1.0.0 \t-  Builds Cellery Registry for version 1.0.0"
     echo
     exit 1
@@ -36,14 +31,14 @@ function showUsageAndExit() {
 
 function showErrorAndExit() {
     if [[ -z "$1" ]]; then
-        echo "${RED}${BOLD}[FATAL] Error message is required${NC}"
+        echo "[FATAL] Error message is required"
         exit 1
     fi
     if [[ -z "$2" ]]; then
-        echo "${RED}${BOLD}[FATAL] Error status is required${NC}"
+        echo "[FATAL] Error status is required"
         exit 1
     fi
-    echo "${RED}[ERROR]$1${NC}"
+    echo "[ERROR]$1"
     exit $2
 }
 
@@ -62,29 +57,10 @@ while getopts :v:h FLAG; do
 done
 
 if [[ -z "$BUILD_VERSION" ]]; then
-    echo "${YELLOW}${BOLD}Build version is not specified. Default version (0.0.1) will be used.${NC}"
+    echo "Build version is not specified. Default version (0.0.1) will be used."
     BUILD_VERSION="0.0.1"
 fi
 
-echo "Building Registry API ..."
-ballerina build registry.api
-
-STATUS=$?
-
-if [[ ${STATUS} != 0 ]]; then
-    showErrorAndExit "Registry API build failed." ${STATUS}
-fi
-
-echo "Building Registry Ballerina Native module ..."
-pushd registry-natives >/dev/null 2>&1
-mvn clean install
-popd >/dev/null 2>&1
-
-STATUS=$?
-
-if [[ ${STATUS} != 0 ]]; then
-    showErrorAndExit "Registry Ballerina Native module build failed." ${STATUS}
-fi
 
 echo
 echo "Building Docker images ..."

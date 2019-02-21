3bb1afba612b8d52e2e49226e8d1c99123ac947b
MadhukaHarith <madhukaw@wso2.com>
2019-02-21 10:26:47 +0530

    Add GCP to CLI setup command

--- components/cli/pkg/commands/setup.go
+++ components/cli/pkg/commands/setup.go
@@ -22,15 +22,25 @@ import (
 	"bufio"
 	"encoding/json"
 	"fmt"
-	"github.com/cellery-io/sdk/components/cli/pkg/constants"
-	"github.com/cellery-io/sdk/components/cli/pkg/util"
-	"github.com/fatih/color"
-	"github.com/manifoldco/promptui"
+	"io"
+	"log"
 	"os"
 	"os/exec"
 	"path/filepath"
 	"strings"
 	"time"
+
+	"cloud.google.com/go/storage"
+	"github.com/cellery-io/sdk/components/cli/pkg/constants"
+	"github.com/cellery-io/sdk/components/cli/pkg/util"
+	"github.com/fatih/color"
+	"github.com/manifoldco/promptui"
+	sqladmin "google.golang.org/api/sqladmin/v1beta4"
+
+	"context"
+
+	"golang.org/x/oauth2/google"
+	"google.golang.org/api/container/v1"
 )
 
 func RunSetup() {
@@ -52,16 +62,20 @@ func RunSetup() {
 	}
 
 	switch value {
-		case constants.CELLERY_SETUP_MANAGE: {
+	case constants.CELLERY_SETUP_MANAGE:
+		{
 			manageEnvironment()
 		}
-		case constants.CELLERY_SETUP_CREATE: {
+	case constants.CELLERY_SETUP_CREATE:
+		{
 			createEnvironment()
 		}
-		case constants.CELLERY_SETUP_SWITCH: {
+	case constants.CELLERY_SETUP_SWITCH:
+		{
 			selectEnvironment()
 		}
-		default: {
+	default:
+		{
 			os.Exit(1)
 		}
 	}
@@ -183,7 +197,8 @@ func createEnvironment() error {
 	}
 
 	switch value {
-		case constants.CELLERY_CREATE_LOCAL: {
+	case constants.CELLERY_CREATE_LOCAL:
+		{
 			vmLocation := filepath.Join(util.UserHomeDir(), ".cellery", "vm")
 			repoCreateErr := util.CreateDir(vmLocation)
 			if repoCreateErr != nil {
@@ -199,10 +214,12 @@ func createEnvironment() error {
 			util.DownloadFromS3Bucket(constants.AWS_S3_BUCKET, constants.AWS_S3_ITEM_CONFIG, vmLocation)
 			installVM()
 		}
-		case constants.CELLERY_CREATE_GCP: {
-			fmt.Println("Installing GCP")
+	case constants.CELLERY_CREATE_GCP:
+		{
+			createGcp()
 		}
-		default: {
+	default:
+		{
 			RunSetup()
 		}
 	}
@@ -251,17 +268,20 @@ func manageEnvironment() error {
 	}
 
 	switch value {
-		case constants.CELLERY_MANAGE_STOP: {
+	case constants.CELLERY_MANAGE_STOP:
+		{
 			spinner := util.StartNewSpinner("Stopping Cellery Runtime")
 			defer func() {
 				spinner.Stop(true)
 			}()
 			util.ExecuteCommand(exec.Command("VBoxManage", "controlvm", constants.VM_NAME, "acpipowerbutton"), "Error stopping VM")
 		}
-		case constants.CELLERY_MANAGE_START: {
+	case constants.CELLERY_MANAGE_START:
+		{
 			util.ExecuteCommand(exec.Command("VBoxManage", "startvm", constants.VM_NAME, "--type", "headless"), "Error starting VM")
 		}
-		case constants.CELLERY_MANAGE_CLEANUP: {
+	case constants.CELLERY_MANAGE_CLEANUP:
+		{
 			spinner := util.StartNewSpinner("Removing Cellery Runtime")
 			defer func() {
 				spinner.Stop(true)
@@ -274,7 +294,8 @@ func manageEnvironment() error {
 			}
 			util.ExecuteCommand(exec.Command("VBoxManage", "unregistervm", constants.VM_NAME, "--delete"), "Error deleting VM")
 		}
-		default: {
+	default:
+		{
 			RunSetup()
 		}
 	}
@@ -334,7 +355,7 @@ func getManageEnvOptions() []string {
 	return []string{constants.CELLERY_SETUP_BACK}
 }
 
-func isVmInstalled () bool {
+func isVmInstalled() bool {
 	cmd := exec.Command("vboxmanage", "list", "vms")
 	stdoutReader, _ := cmd.StdoutPipe()
 	stdoutScanner := bufio.NewScanner(stdoutReader)
@@ -366,4 +387,198 @@ func getCreateEnvironmentList() []string {
 		return []string{constants.CELLERY_CREATE_GCP, constants.CELLERY_SETUP_BACK}
 	}
 	return []string{constants.CELLERY_CREATE_LOCAL, constants.CELLERY_CREATE_GCP, constants.CELLERY_SETUP_BACK}
+}
+
+func createGcp() error {
+	os.Setenv("GOOGLE_APPLICATION_CREDENTIALS", filepath.Join(util.UserHomeDir(), ".cellery", "gcp", "vick-team-1abab5311d43.json"))
+	ctx := context.Background()
+
+	// See https://cloud.google.com/docs/authentication/.
+	// Use GOOGLE_APPLICATION_CREDENTIALS environment variable to specify
+	// a service account key file to authenticate to the API.
+	hc, err := google.DefaultClient(ctx, container.CloudPlatformScope)
+	if err != nil {
+		fmt.Printf("Could not get authenticated client: %v", err)
+	}
+
+	svc, err := container.New(hc)
+	if err != nil {
+		fmt.Printf("Could not initialize gke client: %v", err)
+	}
+
+	errCreate := createGcpCluster(svc)
+	if errCreate != nil {
+		fmt.Printf("Could not create cluster: %v", errCreate)
+	}
+	updateKubeConfig()
+
+	// Create an http.Client that uses Application Default Credentials.
+	hc_sql, err := google.DefaultClient(ctx, sqladmin.CloudPlatformScope)
+
+	if err != nil {
+		fmt.Printf("Error creating client: %v", err)
+	}
+	fmt.Print(hc_sql)
+
+	// Create the Google Cloud SQL service.
+	sqlService, err := sqladmin.New(hc_sql)
+	if err != nil {
+		fmt.Printf("Error creating sql service: %v", err)
+	}
+
+	fmt.Print(sqlService)
+
+	//Create Sql instance
+	_, err = createSqlInstance(ctx, sqlService, constants.GCP_PROJECT_NAME, constants.GCP_REGION, constants.GCP_REGION, constants.GCP_DB_INSTANCE_NAME)
+	if err != nil {
+		log.Print(err)
+	}
+
+	client, err := storage.NewClient(ctx)
+	if err != nil {
+		fmt.Printf("Error creating storage client: %v", err)
+	}
+
+	if err := create(client, constants.GCP_PROJECT_NAME, constants.GCP_BUCKET_NAME); err != nil {
+		fmt.Printf("Error creating storage client: %v", err)
+	}
+
+	if err := uploadSqlFile(client, constants.GCP_BUCKET_NAME, "init.sql"); err != nil {
+		fmt.Printf("Error Uploading Sql file: %v", err)
+	}
+
+	return nil
+}
+func createGcpCluster(svc *container.Service) error {
+	gcpGKENode := &container.NodeConfig{
+		ImageType:   "cos_containerd",
+		MachineType: "n1-standard-1",
+		DiskSizeGb:  12,
+	}
+	gcpCluster := &container.Cluster{
+		Name:                  constants.GCP_CLUSTER_NAME,
+		Description:           "k8s cluster",
+		InitialClusterVersion: "latest",
+		EnableKubernetesAlpha: true,
+		InitialNodeCount:      1,
+		Location:              constants.GCP_ZONE,
+		NodeConfig:            gcpGKENode,
+	}
+	createClusterRequest := &container.CreateClusterRequest{
+		ProjectId: constants.GCP_PROJECT_NAME,
+		Zone:      constants.GCP_ZONE,
+		Cluster:   gcpCluster,
+	}
+
+	k8sCluster, err := svc.Projects.Zones.Clusters.Create(constants.GCP_PROJECT_NAME, constants.GCP_ZONE, createClusterRequest).Do()
+	spinner := util.StartNewSpinner("Creating GCP cluster")
+	defer func() {
+		spinner.Stop(true)
+	}()
+	var clusterStatus string
+	for i := 0; i < 30; i++ {
+		clusterStatus, err = getClusterState(svc, constants.GCP_PROJECT_NAME, constants.GCP_ZONE, constants.GCP_CLUSTER_NAME)
+		if err != nil {
+			fmt.Printf("Error creating cluster : %v", err)
+			break
+		} else {
+			if clusterStatus == "RUNNING" {
+				fmt.Printf("Cluster: %v created", k8sCluster.Name)
+				break
+			}
+			time.Sleep(10 * time.Second)
+		}
+	}
+	if err != nil {
+		return fmt.Errorf("failed to list clusters: %v", err)
+	}
+	fmt.Printf("%s", k8sCluster.Name)
+
+	return nil
+}
+
+func getClusterState(svc *container.Service, projectID, zone, clusterId string) (string, error) {
+	status := ""
+	rsp, err := svc.Projects.Zones.Clusters.Get(projectID, zone, clusterId).Do()
+	if err != nil {
+		status = rsp.Status
+	}
+
+	return status, err
+}
+
+func createSqlInstance(ctx context.Context, service *sqladmin.Service, projectId string, region string, zone string, instanceName string) ([]*sqladmin.DatabaseInstance, error) {
+	settings := &sqladmin.Settings{
+		DataDiskSizeGb: 12,
+		Tier:           "db-f1-micro",
+	}
+
+	dbInstance := &sqladmin.DatabaseInstance{
+		Name:            instanceName,
+		Project:         projectId,
+		Region:          region,
+		GceZone:         zone,
+		BackendType:     "SECOND_GEN",
+		DatabaseVersion: "MYSQL_5_7",
+		MaxDiskSize:     15,
+		Settings:        settings,
+	}
+
+	if _, err := service.Instances.Insert(projectId, dbInstance).Do(); err != nil {
+		fmt.Printf("Error in sql instance creation")
+		log.Fatal(err)
+	}
+	return nil, nil
+}
+
+func create(client *storage.Client, projectID, bucketName string) error {
+	ctx := context.Background()
+	if err := client.Bucket(bucketName).Create(ctx, projectID, nil); err != nil {
+		return err
+	}
+	// [END create_bucket]
+	return nil
+}
+
+func uploadSqlFile(client *storage.Client, bucket, object string) error {
+	ctx := context.Background()
+	util.ReplaceInFile(filepath.Join(util.UserHomeDir(), ".cellery", "gcp", "sql", "init.sql"), "DATABASE_USERNAME", constants.GCP_SQL_USER_NAME)
+	util.ReplaceInFile(filepath.Join(util.UserHomeDir(), ".cellery", "gcp", "sql", "init.sql"), "DATABASE_PASSWORD", constants.GCP_SQL_PASSWORD)
+	f, err := os.Open(filepath.Join(util.UserHomeDir(), ".cellery", "gcp", "sql", "init.sql"))
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+
+	wc := client.Bucket(bucket).Object(object).NewWriter(ctx)
+	if _, err = io.Copy(wc, f); err != nil {
+		return err
+	}
+	if err := wc.Close(); err != nil {
+		return err
+	}
+	return nil
+}
+
+func updateKubeConfig() {
+	cmd := exec.Command("bash", "-c", "gcloud container clusters get-credentials "+constants.GCP_CLUSTER_NAME+" --zone "+constants.GCP_ZONE+" --project "+constants.GCP_PROJECT_NAME)
+	stderr, err := cmd.StderrPipe()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Start command
+	if err = cmd.Start(); err != nil {
+		log.Fatal(err)
+	}
+
+	// prevent main() to exit before cmd completes
+	defer cmd.Wait()
+
+	// read cmd output and send it to stdout
+	// repalce os.Stderr as per your need
+	go io.Copy(os.Stdout, stderr)
+
+	fmt.Println("Standby to read...")
+	fmt.Println()
 }

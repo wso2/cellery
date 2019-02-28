74a041caad472d32bd2a6b2d17c7d45e03612f01
shairam <shaikrish27@gmail.com>
2019-02-28 22:48:33 +0530

    Refactor code and change directory name

--- samples/pet-store/components/orders/server.js
+++ samples/pet-store/components/orders/server.js
@@ -1,4 +1,19 @@
-//Import modules for reading files
+/*
+ *  Copyright (c) 2019, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+// Pet-store sample's orders microservice. Returns a set of order details. 
+
 var fs = require('fs');
 const express = require('express');
 const orderListFile = 'orderlist.json';
@@ -16,8 +31,7 @@ fs.readFile(orderListFile, 'utf8', function (err, data) {
 
 });
 
-app.get('/getOrders', (req, res) => res.send(obj));
-
+app.get('/orders', (req, res) => res.send(obj));
 app.listen(port, () => console.log(`Order-service app listening on port ${port}!`));
 
 

2f4fb5adb307d1177b87e47eacf27bbcebac1ce9
anuruddhal <anuruddhal@wso2.com>
2019-06-24 12:18:22 +0530

    Fix cellery build without Ballerina installed

--- components/cli/pkg/constants/const.go
+++ components/cli/pkg/constants/const.go
@@ -151,4 +151,4 @@ const CELLERY_SQL_PASSWORD = "cellery-sql-user"
 const INGRESS_MODE_NODE_PORT = "Node port [kubeadm, minikube]"
 const INGRESS_MODE_LOAD_BALANCER = "Load balancer [gcp, docker for desktop]"
 
-const CELLERY_RELEASE_VERSION = "0.2.1"
+const CELLERY_RELEASE_VERSION = "0.2.2-SNAPSHOT"

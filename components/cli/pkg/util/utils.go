fc04d19fdd1fdf6d43d2d19f078f9478097cd981
hasinthaindrajee <hasinthaindrajee@gmail.com>
2019-12-16 12:53:25 +0530

    Fix issue of setup not working when minikube is not installed

--- components/cli/pkg/util/utils.go
+++ components/cli/pkg/util/utils.go
@@ -395,8 +395,8 @@ func ConvertToAlphanumeric(input, replacement string) string {
 }
 
 func IsCommandAvailable(name string) bool {
-	cmd := exec.Command("/bin/sh", "-c", "command -v "+name)
-	if err := cmd.Run(); err != nil {
+	_, err := exec.LookPath(name)
+	if err != nil {
 		return false
 	}
 	return true
